eb43d3dc0b228ff712b8018108c92be947019c06
MorrisRoberti <morrisroberti349@gmail.com>
2024-03-01 20:31:05 +0000

    creato struttura della creazione hour_log, da implementare le funzioni nel service

--- src/endpoints/hour-logs/hour-logs.controller.ts
+++ src/endpoints/hour-logs/hour-logs.controller.ts
@@ -8,39 +8,94 @@ import {
   Delete,
   UseGuards,
   UseFilters,
+  Request,
+  HttpStatus,
 } from '@nestjs/common';
 import { HourLogsService } from './hour-logs.service';
 import { CreateHourLogDto } from './dto/create-hour-log.dto';
 import { UpdateHourLogDto } from './dto/update-hour-log.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { DBExceptionFilter } from 'src/error_handling/db.exception.filter';
+import { WeeklyLogTable } from 'src/db/models/weekly-log.model';
+import { Sequelize } from 'sequelize-typescript';
 
 @UseGuards(AuthGuard('jwt'))
 @UseFilters(DBExceptionFilter)
 @Controller('hour-logs')
 export class HourLogsController {
-  constructor(private readonly hourLogsService: HourLogsService) {}
+  constructor(
+    private readonly hourLogsService: HourLogsService,
+    private sequelize: Sequelize,
+  ) {}
 
   @Post()
-  create(@Body() createHourLogDto: CreateHourLogDto) {}
+  async create(
+    @Request() request: any,
+    @Body() createHourLogDto: CreateHourLogDto,
+  ) {
+    // check if the weekly_log record for user already exists
+    const isWeeklyLogPresent = await this.hourLogsService.isWeeklyLogPresent(
+      request.user.id,
+      createHourLogDto.date,
+    );
 
-  @Get()
-  findAll() {
-    return this.hourLogsService.findAll();
+    let weeklyLog: WeeklyLogTable;
+
+    const transaction = await this.sequelize.transaction();
+
+    if (isWeeklyLogPresent) {
+      // find associated weekly_log record
+      weeklyLog = await this.hourLogsService.findWeeklyLog(
+        request.user.id,
+        createHourLogDto.date,
+      );
+
+      // convert weekly_log record (with the createdHourLogDto hours and minutes)
+      const convertedWeeklyLog = this.hourLogsService.addHoursToWeeklyLog(
+        weeklyLog,
+        createHourLogDto.hours,
+        createHourLogDto.minutes,
+      );
+
+      // update weekly_log record on db
+      await this.hourLogsService.updateWeeklyLog(
+        convertedWeeklyLog,
+        transaction,
+      );
+    } else {
+      // convert new weekly_log record
+      const weeklyLogConverted = this.hourLogsService.convertNewWeeklyLog(
+        request.user.id,
+        createHourLogDto,
+      );
+      // create new weekly_log record on db
+      weeklyLog = await this.hourLogsService.createWeeklyLog(
+        weeklyLogConverted,
+        transaction,
+      );
+    }
+
+    // convert hour_log (with the weekly_log_id)
+    const convertedHourLog = this.hourLogsService.convertNewHourLog(
+      request.user.id,
+      weeklyLog.id,
+      createHourLogDto,
+    );
+    // create hour_log on db
+    await this.hourLogsService.createHourLog(convertedHourLog, transaction);
+    await transaction.commit();
+
+    // return
+    return HttpStatus.CREATED;
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.hourLogsService.findOne(+id);
-  }
+  // GET week hours
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateHourLogDto: UpdateHourLogDto) {
-    return this.hourLogsService.update(+id, updateHourLogDto);
-  }
+  // GET hour_log from id
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.hourLogsService.remove(+id);
-  }
+  // GET weekly log for subject from subject id
+
+  // PUT hour_log from id
+
+  // DELETE hour_log from id
 }
--- src/endpoints/hour-logs/hour-logs.service.ts
+++ src/endpoints/hour-logs/hour-logs.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateHourLogDto } from './dto/create-hour-log.dto';
 import { UpdateHourLogDto } from './dto/update-hour-log.dto';
+import { WeeklyLogTable } from 'src/db/models/weekly-log.model';
+import { Op } from 'sequelize';
+import { HourLogTable } from 'src/db/models/hour-log.model';
 
 @Injectable()
 export class HourLogsService {
-  create(createHourLogDto: CreateHourLogDto) {
-    return 'This action adds a new hourLog';
+  constructor(private logger: Logger) {}
+
+  WEEKLY_LOG = 'WeeklyLog';
+  HOUR_LOG = 'HourLog';
+
+  addHoursToWeeklyLog(
+    weeklyLog: WeeklyLogTable,
+    hours: number,
+    minutes: number,
+  ) {}
+
+  convertNewWeeklyLog(user_id: number, createdHourLogDto: CreateHourLogDto) {}
+
+  convertNewHourLog(
+    user_id: number,
+    weekly_log_id: number,
+    createdHourLogDto: CreateHourLogDto,
+  ) {}
+
+  // db functions
+  async isWeeklyLogPresent(user_id: number, date: string): Promise<boolean> {
+    this.logger.log(`Check if ${this.WEEKLY_LOG} is present`);
+    const isPresent = await WeeklyLogTable.findOne({
+      where: {
+        [Op.and]: {
+          user_id,
+          week_start: { [Op.gte]: date },
+          week_end: { [Op.lte]: date },
+        },
+      },
+    });
+
+    if (isPresent && isPresent !== null) {
+      return true;
+    }
+
+    return false;
   }
 
-  findAll() {
-    return `This action returns all hourLogs`;
+  async findWeeklyLog(user_id: number, date: string): Promise<WeeklyLogTable> {
+    return new WeeklyLogTable(); // placeholder
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} hourLog`;
+  async updateWeeklyLog(weeklyLog: any, transaction: any) {}
+
+  async createWeeklyLog(weeklyLog: any, transaction): Promise<WeeklyLogTable> {
+    return new WeeklyLogTable(); // placeholder
   }
 
-  update(id: number, updateHourLogDto: UpdateHourLogDto) {
-    return `This action updates a #${id} hourLog`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} hourLog`;
+  async createHourLog(hourLog: any, transaction: any): Promise<HourLogTable> {
+    return new HourLogTable(); // placeholder
   }
 }
